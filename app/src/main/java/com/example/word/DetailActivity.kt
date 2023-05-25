d86082b299f15563239b0a2c5a22106a6356bfe7
Alitor004 <irmaosgomes1302@gmail.com>
2023-05-24 21:42:35 -0300

    Criação do Words

---
+++ app/src/main/java/com/example/word/DetailActivity.kt
@@ -0,0 +1,33 @@
+package com.example.word
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.word.databinding.ActivityDetailBinding
+import com.example.wordsapp.WordAdapter
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val letterId = intent?.extras?.getString("letter").toString()
+
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = WordAdapter(letterId, this)
+
+
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        )
+        title = getString(R.string.detail_prefix) + " " + letterId
+    }
+    companion object {
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q=linguagem+"
+    }
+}
---
+++ app/src/main/java/com/example/word/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.word
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.word.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = LetterAdapter()
+    }
+}
